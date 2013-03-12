9f813a36c4350d4d4a58c7baefb6d56f8aab6b50
xu tao <xutao@xumatoMacBook-Pro.local>
2013-03-12 15:59:19 +0800

    Initial Commit

---
+++ qdexpo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  qdexpo
+//
+//  Created by tao xu on 13-3-12.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, nil);
+    }
+}
---
+++ qdexpo/qdexpoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  qdexpoAppDelegate.h
+//  qdexpo
+//
+//  Created by tao xu on 13-3-12.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface qdexpoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
